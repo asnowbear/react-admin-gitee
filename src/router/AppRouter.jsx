f7adffd177a81fc4285ad236ee7c093336ff9610
zhangyong900689 <1106408264@qq.com>
2020-09-27 18:12:18 +0800

    note：withRouter的作用是将路由参数传入到非路由加载的组件总

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom' // 切换路由的Switch
 import {isLogin} from '@/commons';
 import PageFrame from '@/layouts/frame';
 import Error404 from '@/pages/error/Error404';
